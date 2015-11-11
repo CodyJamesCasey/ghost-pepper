91105badf6d2b9c34499aeb10060249b40bceff8
Cody James Casey <codyjamescasey@gmail.com>
2015-11-11 15:21:28 -0500

    super simple socket.io example works

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,14 @@ var gulp    = require('gulp'),
     nodemon = require('gulp-nodemon'),
     path    = require('path');
 
+require('./sips/html')(gulp);
+require('./sips/browserify')(gulp);
+
 gulp.task('server', function() {
   nodemon({
     script: path.join(__dirname, 'server', 'index.js'),
     ignore: ['client/*', 'gulpfile.js']
   });
 });
+
+gulp.task('default', ['html', 'browserify', 'server']);
